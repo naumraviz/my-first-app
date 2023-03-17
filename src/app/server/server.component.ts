417c330a0d38b7719e1e00c7f066a2431e176cb3
nraviz <nraviz@tenable.com>
2023-03-17 13:22:05 +0200

    commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,26 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerCompnonet {
-    serverID: number = 10;
-    serverStatus: string = "offline";
+    serverID = 10;
+    serverStatus = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus(): string {
         return this.serverStatus
+    }
+
+    getColor(): string {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
